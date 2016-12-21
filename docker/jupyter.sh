af8003f1eb0523dfcaf722088c0185312babed2d
Bradley Skaggs <bskaggs@acm.org>
2016-12-21 00:20:41 -0500

    Sane defaults for jupyter

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+FOUND=false
 for HOMEDIR in /home/* ; do 
   if [[ -d "$HOMEDIR" && ! -L "$HOMEDIR" ]]; then
     NEWUSER=$(basename "$HOMEDIR")
     NEWUID=$(stat -c %u "$HOMEDIR")
     NEWGID=$(stat -c %g "$HOMEDIR")
     echo "Creating new user with name $NEWUSER, uid $NEWUID, gid $NEWGID"
-    groupadd -g $NEWGID group
-    useradd -g $NEWGID -u $NEWUID -M -d "$HOMEDIR" "$NEWUSER"
-    cd "$HOMEDIR"
-    exec su "$NEWUSER" -c 'jupyter-notebook --ip 0.0.0.0 --no-browser'
+    groupadd -g "$NEWGID" group
+    useradd -g "$NEWGID" -u "$NEWUID" -M -d "$HOMEDIR" "$NEWUSER"
+    cd "$HOMEDIR" || exit
+    FOUND=true
+    break
   fi 
 done
-echo "No user home directory found, making user named 'user'."
-useradd -m user
-cd /home/user
-exec su user -c 'jupyter-notebook --ip 0.0.0.0 --no-browser'
 
+if [ "$FOUND" = false ]; then
+  NEWUSER=user
+  echo "No user home directory found, making user named 'user'."
+  useradd -m "$NEWUSER"
+  cd "/home/$NEWUSER" || exit
+fi
+
+exec su "$NEWUSER" -c 'jupyter-notebook --ip 0.0.0.0 --no-browser'
